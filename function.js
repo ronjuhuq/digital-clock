2cccd8907af9a15e6bd18c4792d2b1557bb7a728
ronjuhuq <ronjuahmed2424@gmail.com>
2022-01-09 22:50:00 +0600

    all file upload

--- function.js
+++ function.js
@@ -1,30 +1,21 @@
-const clock = document.querySelector('.clock-wrap');
-
-const h = clock.querySelector('#h');
-const m = clock.querySelector('#m');
-const s = clock.querySelector('#s');
-const amp = clock.querySelector('#amp');
-
-setInterval(() => {
-
-     let date = new Date();
-
-let hour = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-
-if (h > 12) {
-
-     amp.innerHTML = "AM";
-     
-}else{
-     amp.innerHTML = "PM";
-}
-
-h.innerHTML = `${hour < 10 ? "0" + hour : hour }:`;
-m.innerHTML = `${min < 10 ? "0" + min : min }:`;
-s.innerHTML = `${sec < 10 ? "0" + sec : sec }`;
-
-
-     
-}, 1000);
+function wick(day){
+     if(day == 0){
+         return 'sunday';
+     }else if(day == 1){
+         return 'monday';
+     }else if(day == 2){
+         return 'tuesday';
+     }else if(day == 3){
+         return 'wednesdy';
+     }else if(day == 4){
+         return 'thursday';
+     }else if(day == 5){
+         return 'friday';
+     }else if(day == 6){
+         return 'saturday';
+     };
+ };
+ 
+ function zero(time){
+     return time < 10 ? '0' + time : time;  
+ }
